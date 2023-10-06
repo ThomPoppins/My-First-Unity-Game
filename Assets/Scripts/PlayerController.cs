daf7baf832c523d7f4b6c97b3643fe294d9b125d
Thom Veldpaus <thompoppins@gmail.com>
2023-10-06 09:51:05 +0100

    Add player movement, left and right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontalBoundary = 8.0f; // Horizontal boundaries
+    private float speed = 20.0f; // Player speed
+    private Rigidbody playerRb; // Player rigidbody
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get player rigidbody
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move player left and right with arrow keys or A and D within the left and right boundaries from the input manager
+        if (Input.GetAxis("Horizontal") > 0 && transform.position.x < horizontalBoundary)
+        {
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        }
+        else if (Input.GetAxis("Horizontal") < 0 && transform.position.x > -horizontalBoundary)
+        {
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        }
     }
 }
